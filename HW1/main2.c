193b3500114950111a0a9acece23657724870e8a
Henry <hjinshan0813@gmail.com>
2023-02-28 16:26:11 +0800

    Modified main2.c and main4.c

--- HW1/main2.c
+++ HW1/main2.c
@@ -2,9 +2,12 @@
 
 int main()
 {
-    char a[]="ABCabcABC";
-    int shift=4,array_size=sizeof(a)-1;
-    for(int i=shift;i<array_size;i++){
-        printf("%c",a[i]);
+    char a[]="A3B2C4A9";
+    int array_size=sizeof(a)-1;
+    for(int n=0;n<array_size;n+=2){
+        int count=a[n+1]-'0';
+        for(int j=0;j<count;j++){
+            printf("%c",a[n]);
+        }
     }
 }
--- HW1/main4.c
+++ HW1/main4.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
-#include <math.h>
+
 int main()
 {
-    char a[]="ac-99bc";
+    char a[]="ac-789bc";
     int array_size=sizeof(a)-1,sum=0,count=-1,x=0;
+    
     for(int i=0;i<array_size;i++){
         if(a[i]=='-'){
             x=1;
         }
     }
     
-    for(int i=0;i<array_size;i++){
-        if(a[i]<65&&a[i]!='-'&&x!=1){
+    for(int i=array_size;i>0;i--){
+        if(a[i-1]<65&&x!=1){
             count++;
-            int temp=a[i]-'0';
-            sum=sum+temp*pow(10,count);
-        }else if(a[i]<65){
-            if(a[i]=='-'){
+            int temp=a[i-1]-'0',temp_pow; 
+            
+            if(count==0){
+                temp_pow=temp;
+            }else{
+                for(int j=0;j<count;j++){
+                temp_pow=temp*=10;
+                }
+            }
+            
+            sum=sum+temp_pow;
+            printf("%d\n",sum);
+        }else if(a[i-1]<65){
+            if(a[i-1]=='-'){
                 int b=0;
             }else{
-            count++;
-            int temp=a[i]-'0';
-            sum=sum-temp*pow(10,count);
+             count++;
+             int temp=a[i-1]-'0',temp_pow; 
+            if(count==0){
+                temp_pow=temp;
+            }else{
+                for(int j=0;j<count;j++){
+                  temp_pow=temp*=10;
+                }
             }
+            sum=sum-temp_pow;
+          }
         }
     }
     
