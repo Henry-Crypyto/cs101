68f8251305a43b660e0f8f8c1bc268633d5cfd3a
Henry <hjinshan0813@gmail.com>
2023-03-14 10:00:44 +0800

    Finish main2.c

---
+++ HW2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	FILE  *fp= fopen("main.c", "r+"), *fpw = fopen("main2.txt", "w+");
+	int c, row = 1;
+	
+	fprintf(fpw,"01  ");
+	while ((c = getc(fp)) != EOF)
+	    if(c=='\n'){
+	        fprintf(fpw,"\n%02d  ", ++row);
+	    }else{
+	        fprintf(fpw,"%c", c);
+	    }
+	fclose(fp);
+	fclose(fpw);
+	return 0;
+}
